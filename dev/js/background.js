03dc484de0298b9002e0ee9ad260edc77e18e691
bbenefield89 <bsquared18@gmail.com>
2018-08-09 21:45:06 -0400

    MOVE `background.js`
    in ./dev/js
    moved `background.js` into `./dev/js/`

---
+++ dev/js/background.js
@@ -0,0 +1,10 @@
+const rules = {
+  conditions: [ new chrome.declarativeContent.PageStateMatcher({ pageUrl: { schemes: [ 'https', 'http' ] } }) ],
+  actions: [ new chrome.declarativeContent.ShowPageAction() ]
+};
+
+chrome.runtime.onInstalled.addListener(details => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([ rules ])
+  });
+});
