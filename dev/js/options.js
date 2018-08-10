52719910322a63d7f7b30157b76a944eaaec8e74
bbenefield89 <bsquared18@gmail.com>
2018-08-09 21:55:24 -0400

    RENDER LIST OF SITES
    - in options.js
    render list of sites in `localStorage`

--- dev/js/options.js
+++ dev/js/options.js
@@ -1,8 +1,13 @@
 const blackListedSitenamesList = document.querySelector('.blacklisted_sitenames_list');
-const blacklistedSitenamesItem = document.createElement('li');
 
-console.log(localStorage)
+if (!localStorage.getItem('sites'))
+  localStorage.setItem('sites', '[]');
 
-for (key in localStorage) {
-  console.log(localStorage[ key ]);
-}
+localStorageSitesList = JSON.parse(localStorage.getItem('sites'));
+
+localStorageSitesList.forEach(val => {
+  const blacklistedSitenamesItem = document.createElement('li');
+
+  blacklistedSitenamesItem.insertAdjacentText('afterbegin', val);
+  blackListedSitenamesList.insertAdjacentElement('afterbegin', blacklistedSitenamesItem);
+});
