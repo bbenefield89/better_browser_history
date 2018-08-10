d7cbe52767090c6b2cc8bb6041ddca1c6dab366b
bbenefield89 <bsquared18@gmail.com>
2018-08-09 23:09:20 -0400

    REMOVE SELECTED ELEMENT FROM DOM/`localStorage`
    - in options.js

--- dev/js/options.js
+++ dev/js/options.js
@@ -1,13 +1,26 @@
 const blackListedSitenamesList = document.querySelector('.blacklisted_sitenames_list');
 
+// create `sites` if it does not already exist in `localStorage`
 if (!localStorage.getItem('sites'))
-  localStorage.setItem('sites', '[]');
+  localStorage.setItem('sites', '{}');
 
-localStorageSitesList = JSON.parse(localStorage.getItem('sites'));
+// parse `sites` from `localStorage`
+// this is an easy way to store an "array" in `localStorage`
+const localStorageSitesList = JSON.parse(localStorage.getItem('sites'));
 
-localStorageSitesList.forEach(val => {
+// add a new site name to list for each item in `localStorage.getItem('sites')`
+for (site in localStorageSitesList) {
   const blacklistedSitenamesItem = document.createElement('li');
 
-  blacklistedSitenamesItem.insertAdjacentText('afterbegin', val);
-  blackListedSitenamesList.insertAdjacentElement('afterbegin', blacklistedSitenamesItem);
-});
+  blacklistedSitenamesItem.insertAdjacentText('afterbegin', localStorageSitesList[ site ]);
+  blackListedSitenamesList.insertAdjacentElement('beforeend', blacklistedSitenamesItem);
+}
+
+for (let i = 0; i < blackListedSitenamesList.children.length; i++) {
+  // add an event listener for each site name in teh list
+  blackListedSitenamesList.children[ i ].addEventListener('click', function (e) {
+    delete localStorageSitesList[ this.innerText ];  // delete site name from object
+    localStorage.setItem('sites', JSON.stringify(localStorageSitesList));
+    blackListedSitenamesList.removeChild(this);  // remove this element from the DOM
+  });
+}
