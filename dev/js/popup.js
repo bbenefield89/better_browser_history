92f6f791e5fb3049f79a8e097ef0c979b38c2690
bbenefield89 <bsquared18@gmail.com>
2018-08-11 15:44:21 -0400

    REMOVE UNNECESSARY CODE
    - in popup.js

--- dev/js/popup.js
+++ dev/js/popup.js
@@ -1,13 +1,5 @@
 const submitSitenameButton  = document.querySelector('.sitename_form__sitename_submit_button');
 const sitenameInput         = document.querySelector('.sitename_form__sitename_input');
-const pattern               = new RegExp('triplebyte', 'i')
-
-const query = {
-  text: '',
-  startTime: 0,
-  endTime: Date.now(),
-  maxResults: 0
-};
 
 // check if `sites` is defined in localStorage
 // if not then create it as an empty array (technically it's a string)
@@ -26,13 +18,3 @@ submitSitenameButton.addEventListener('click', e => {
   localStorageSitesList[ sitenameInputVal ] = sitenameInputVal;
   localStorage.setItem('sites', JSON.stringify(localStorageSitesList));
 });
-
-// chrome.history.search(query, history => {
-//   history.forEach(website => {
-//     console.log(website.url + '\n')
-    
-//     if (website.url.match(pattern)) {
-//       console.log(website)
-//     }
-//   });
-// });
